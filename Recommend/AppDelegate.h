1fec029205eb949da0f748046b53a04beddec160
thomasorten <thomas.orten@gmail.com>
2014-06-16 15:01:19 -0500

    Initial Commit

---
+++ Recommend/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Recommend
+//
+//  Created by Thomas Orten on 6/16/14.
+//  Copyright (c) 2014 Orten, Thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Recommend/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Recommend
+//
+//  Created by Thomas Orten on 6/16/14.
+//  Copyright (c) 2014 Orten, Thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
