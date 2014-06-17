35184f7b9137fdcd81d6d4ff32ec12d362eeeca9
Dan Rudolf <danrudolf@gmail.com>
2014-06-17 15:03:36 -0500

    Added Facebook Framework

---
+++ Recommend/LoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.h
+//  Recommend
+//
+//  Created by Dan Rudolf on 6/17/14.
+//  Copyright (c) 2014 Orten, Thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <FacebookSDK/FacebookSDK.h>
+
+
+@interface LoginViewController : UIViewController
+
+@end
